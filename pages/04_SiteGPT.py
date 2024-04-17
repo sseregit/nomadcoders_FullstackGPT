7f3ff6fa5f8492daf95066bc5e0a2e59b855b826
Jangyeonsik <sseregit@naver.com>
2024-04-17 15:43:44 +0900

    10.3 Parsing Function

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from langchain.document_loaders import SitemapLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 st.set_page_config(
     page_title="SiteGPT",
@@ -17,11 +18,30 @@ st.markdown("""
 with st.sidebar:
     url = st.text_input("Write down a URL", placeholder="https://example.com")
 
+def parse_page(soup):
+    header = soup.find("header")
+    if header:
+        header.decompose()
+    footer = soup.find("footer")
+    if footer:
+        footer.decompose()
+    return str(soup.get_text()).replace("\n", " ").replace("\xa0"," ").replace("CloseSearch Submit Blog", "")
+
 @st.cache_data
 def load_website(url):
-    loader = SitemapLoader(url)
+    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000,
+        chunk_overlap=200,
+    )
+    loader = SitemapLoader(
+        url,
+        filter_urls=[
+            r"^(.*\/blog\/).*",
+        ],
+        parsing_function=parse_page
+    )
     loader.requests_per_second = 5
-    docs = loader.load()
+    docs = loader.load_and_split(text_splitter=splitter)
     return docs
 
 
